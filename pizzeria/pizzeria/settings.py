8f056cf8c7a06e67efb3d2205f051a627d715432
mgonzalz <mgonzgarc@myuax.com>
2024-12-24 02:05:44 +0100

    definición de variables de entorno - claves privadas

--- pizzeria/pizzeria/settings.py
+++ pizzeria/pizzeria/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.getenv('VERCEL') is None and os.getenv('GITHUB_ACTIONS') is None:
+    load_dotenv() # Load environment variables from .env file.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4sj%!ncledd#455**1cjc#uai@r)e7_%v0*_c$ca1p7a!0ou=x'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
