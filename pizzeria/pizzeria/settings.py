0c6b9519b9efd54e4e669993e071965aa9e23df4
mgonzalz <mgonzgarc@myuax.com>
2025-01-03 21:12:27 +0100

    eliminación app `nutrition` - somos dioses y averiguamos cómo usar Recipe Search API sin pagar

--- pizzeria/pizzeria/settings.py
+++ pizzeria/pizzeria/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nutrition',
+
 
     # Third-party apps
     'rest_framework', # Django REST framework - Nice clean views for API.
