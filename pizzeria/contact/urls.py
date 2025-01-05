de20f8460804aca14c8af142a00095c437a2ea36
mgonzalz <mgonzgarc@myuax.com>
2025-01-05 04:12:08 +0100

    página `contact.html` + `base.html`

--- pizzeria/contact/urls.py
+++ pizzeria/contact/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 # URL configuration for pizzeria project (contact application).
 urlpatterns = [
+    path('', views.contact, name='contact'),
 ]
